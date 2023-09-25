c666b642ebe798e1ff19c7603a0fd1152ccb26fd
realsanjeev <realsanjeev2@gmail.com>
2023-09-25 12:39:13 +0545

    adding the app_view

---
+++ todo_task/todos_app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+
+# defining the namespace
+app_name = "todos"
+urlpatterns = [
+    path('', views.GenericListView.as_view(), name="index"),
+    path("<int:todo_id>/delete", views.delete, name="delete-task"),
+    path("<int:todo_id>/update", views.update, name="update-task"),
+    path("add/", views.add, name="add-task")
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ todo_task/todos_app/views.py
@@ -0,0 +1,41 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import generic
+
+from task_api.models import TodoTask
+
+
+class GenericListView(generic.ListView):
+    template_name = "todos/index.html"
+    context_object_name = 'todo_list'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return TodoTask.objects.order_by("updated")
+
+def add(request, *args, **kwargs):
+    task = request.POST.get("task")
+    desc = request.POST.get("desc")
+    if desc is None:
+        desc=task
+    
+    TodoTask.objects.create(task=task, desc=desc)
+    return redirect('todos:index')
+
+def delete(request, *args, **kwargs):
+    id = kwargs.get("todo_id")
+    todo = get_object_or_404(TodoTask, pk=id)
+
+    todo.delete()
+    return redirect("todos:index")
+
+def update(request, *args, **kwargs):
+    id = kwargs.get("todo_id")
+    completed = request.POST.get("completed", False)
+    if completed == "on":
+        completed = True
+    todo = get_object_or_404(TodoTask, pk=id)
+
+    todo.completed = completed
+    todo.save()
+    return ("todos:index")
