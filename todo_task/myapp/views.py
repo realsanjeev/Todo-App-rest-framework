a9460210ae9ddbbc23a7ec6ade85aa973c9580ba
sanjeev.bhandari <sanjeev.bhandari@tai.com.np>
2024-07-12 21:59:36 +0545

    Squashed commit of the following:
    commit 8c7cbf840685b82fccc2c2fb64fe51f3cae68ef0
    Author: sanjeev.bhandari <sanjeev.bhandari@tai.com.np>
    Date:   Fri Jul 12 21:57:53 2024 +0545
    
        Added authentication in app

--- todo_task/myapp/views.py
+++ todo_task/myapp/views.py
@@ -1,18 +1,19 @@
 from typing import Any
 
 from django.contrib import messages
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db.models.query import QuerySet
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views import generic
-from task_api.models import TodoTask
 
+from task_api.models import TodoTask
 from .forms import LoginForm, SignUpForm
 
 
 class GenericListView(generic.ListView):
-    template_name = "todos/index.html"
+    template_name = "myapp/index.html"
     context_object_name = "todo_list"
 
     def get_queryset(self) -> QuerySet[Any]:
@@ -21,41 +22,46 @@ class GenericListView(generic.ListView):
 
 def signup_view(request):
     if request.method == "POST":
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("home")
-    else:
-        form = SignUpForm()
-    return render(request, "account/signup.html", {"form": form})
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
 
+        if User.objects.filter(email=email).exists():
+            messages.error(request, f"A user with email {email} already exists.")
+        elif User.objects.filter(username=username).exists():
+            messages.error(request, f"A user with username {username} already exists.")
+        elif password1 != password2:
+            messages.error(request, "Password and Confirmation password must be the same.")
+        else:
+            user = User.objects.create_user(username=username, email=email, password=password1)
+            messages.success(request, "Your account has been created successfully.")
+            login(request, user)
+            return redirect("todos:login")
+
+    return render(request, "account/signup.html")
 
 def login_view(request):
     if request.method == "POST":
-        form = LoginForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect("home")
-            else:
-                messages.error(request, "Invalid username or password.")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(username, password)
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("todos:index")
         else:
             messages.error(request, "Invalid username or password.")
-    else:
-        form = LoginForm()
-    return render(request, "account/login.html", {"form": form})
 
+    return render(request, "account/login.html")
 
 @login_required
 def logout_view(request):
     logout(request)
-    return redirect("login")
+    return redirect("todos:login")
 
-
+@login_required
 def add(request, *args, **kwargs):
     task = request.POST.get("task")
     desc = request.POST.get("desc")
@@ -65,7 +71,7 @@ def add(request, *args, **kwargs):
     TodoTask.objects.create(task=task, desc=desc)
     return redirect("todos:index")
 
-
+@login_required
 def delete(request, *args, **kwargs):
     id = kwargs.get("todo_id")
     todo = get_object_or_404(TodoTask, pk=id)
@@ -74,6 +80,7 @@ def delete(request, *args, **kwargs):
     return redirect("todos:index")
 
 
+@login_required
 def update(request, *args, **kwargs):
     id = kwargs.get("todo_id")
     completed = request.POST.get("completed", False)
